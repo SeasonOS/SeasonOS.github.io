9d4c005e3a076b10af60a821e76002a9aa8cc258
test <53500312+SeasonOS@users.noreply.github.com>
2021-02-03 21:31:26 +0800

    Add files via upload

--- odyink/Blog/2.bat
+++ odyink/Blog/2.bat
@@ -12,10 +12,10 @@ rem echo CScript脚本解释器 指定对象说话 "说话内容(女声)" 到 peak0.vbs
 pause
 cls
 echo 下载所需软件
-    PowerShell wget https://smgdream.top/wget.exe -outfile wget.exe >nul
-    wget --no-check-certificate -P ./ https://smgdream.top/7z.exe
-rem wget -P(大写)指定下载目录 ./下载目录(unix格式) https://smgdream.top/7z.exe链接
-wget --no-check-certificate -P ./ https://smgdream.top/ffplay.7z
+    PowerShell wget https://smgdata-1302226504.cos.accelerate.myqcloud.com/wget.exe -outfile wget.exe >nul
+    wget --no-check-certificate -P ./ https://smgdata-1302226504.cos.accelerate.myqcloud.com/7z.exe
+    rem wget -P(大写)指定目录 ./下载目录(unix格式) https://smgdata-1302226504.cos.accelerate.myqcloud.com/7z.exe链接
+    wget --no-check-certificate -P ./ https://smgdata-1302226504.cos.accelerate.myqcloud.com/ffplay.7z
 cls
 echo 解压文件中
 rem 7z e解压 ffplay.7z解压目标
@@ -24,11 +24,11 @@ cls
 echo 回车播放音频
 Pause >nul
 rem ffplay -autoexit播放完毕自动退出 5673op.mp3播放目标
-ffplay -autoexit https://smgdream.top/5673op.mp3
+ffplay -autoexit https://smgdata-1302226504.cos.accelerate.myqcloud.com/5673op.mp3
 cls
 echo 回车播放视频
 Pause >nul
-wget --no-check-certificate -P ./ https://smgdream.top/pp.mp4
+wget --no-check-certificate -P ./ https://smgdata-1302226504.cos.accelerate.myqcloud.com/pp.mp4
 ffplay -autoexit pp.mp4
 cls
 echo 回车退出
